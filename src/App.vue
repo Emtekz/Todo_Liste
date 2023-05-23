727f9ba198b2deb92e446ef7d335016a64d21bee
Paul Reiter <reiterpaul02@gmail.com>
2023-05-23 14:43:55 +0200

    YOLOOO!

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="bg-gray-600 text-white w-1/2 m-auto mt-12">
+    <div class="text-center border-b-2 border-black py-4">
+      <h1 class="text-3xl py-4">Unsere Todos</h1>
+      <p class="text-xl" v-if="opentodos.length > 0">Offene To-Do's: {{ opentodos.length }}/{{ todos.length }}</p>
+      <p class="text-xl" v-else>Keine Offenen To-Do's</p>
+
+      <div class="mt-4 px-2">
+        <input
+          type="text"
+          class="py-2 px-1 w-2/3 text-black"
+          v-model="newTodo"
+          id="input-todo"
+        />
+        <button class="bg-red-400 py-2 w-1/3" @click="addTodo">
+          Add To-Do
+        </button>
+      </div>
+    </div>
+
+    <div v-for="(todo, index) in todos" :key="todo.todo">
+      <!-- Übergabe von Werten aus todos Array -->
+      <TodoList
+        :todoprop="todo"
+        :todoindex="index"
+        @toggledone-index="toggleDone"
+        @removetodo-index="deleteTodo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  name: "App",
+
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    }
+  },
+
+  mounted() {
+    let data = localStorage.getItem("todos");
+
+    if(data !== "" && data !== null){
+
+      this.todos = JSON.parse(data);
+    }
+    else {
+      this.todos = [];
+    }
+
+  },
+
+  computed: {
+    opentodos() {
+      const openTodos = this.todos.filter((todo) => {
+        return !todo.done
+      })
+
+      return openTodos;
+    }
+  },
+
+  components: {
+    TodoList,
+  },
+
+  methods: {
+    toggleDone(index) {
+      this.todos[index].done = !this.todos[index].done;
+
+      this.storeTodos();
+    },
+
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+
+      this.storeTodos();
+    },
+
+    addTodo() {
+      if(this.newTodo.trim() === "") {
+        return;
+      }
+      else {
+        this.todos.push({todo: this.newTodo, done: false});
+        document.querySelector('#input-todo').value = "";
+
+        this.storeTodos();
+      }
+    },
+
+    storeTodos() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+  },
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- Dynamische Klassen (nach :class) -->
+  <div class="flex justify-between items-center py-2 px-2" :class="{'bg-green-600': this.todo.done, 'bg-red-600': !this.todo.done}">
+    
+    <h1 class="cursor-pointer" @click="toggleTodo">
+      {{ todo.todo }}
+    </h1>
+
+    <button class="bg-black py-1 px-1 rounded" @click="removeTodo">Delete Todo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+
+  props: ["todoprop", "todoindex"],
+
+  // Variablen 
+  // Können mit {{ varName }} ausgegeben werden
+  data() {
+    return {
+      // Bindung an Property
+      todo: this.todoprop,
+    }
+  },
+
+  // Funktionen
+  // z.B. mit "v-on:click='Fktn'" aufrufen (= onclick)
+  // anstelle von "v-on:" geht auch "@:"
+  // methods: {
+  //   countUp() {
+  //     this.count++;
+  //   }
+  // }
+  methods: {
+    toggleTodo() {
+      this.$emit("toggledone-index", this.todoindex);
+    },
+
+    removeTodo() {
+      this.$emit("removetodo-index", this.todoindex);
+    }
+  },
+}
+</script>
+
